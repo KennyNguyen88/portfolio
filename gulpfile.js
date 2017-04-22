b5a7c0fc7c3cbb1ac6f5d7a7ce010d99340f1c97
KennyNguyen88 <giatrangrua@gmail.com>
2017-04-22 19:29:38 +0700

    Update Workflow Update Page Tera & Tera Landing

--- gulpfile.js
+++ gulpfile.js
@@ -7,114 +7,119 @@ var gulp = require('gulp'),
     minifyHTML = require('gulp-minify-html'),
     jsonminify = require('gulp-jsonminify'),
     concat = require('gulp-concat'),
-    typescript = require('gulp-typescript'),
     webserver = require('gulp-webserver'),
-    sourcemaps = require('gulp-sourcemaps'),
-    tscConfig = require('./tsconfig.json')
-    ;
+    sourcemaps = require('gulp-sourcemaps')
+
 var env,
-    jsSrc,
-    sassSrc,
-    htmlSrc,
-    jsonSrc,
     outputDir,
+    htmlSrc,
+    sassSrc,
+    jsSrc,
+    jsonSrc,
+    fontSrc,
     sassStyle,
-    bootstrap,
-    tsSrc;
+    bootstrap;
+
+
 env = process.env.NODE_ENV || 'development';
+outputDir = 'builds/development/';
 htmlSrc = [outputDir + '*.html'];
+sassSrc = 'components/sass/styles.scss';
+jsSrc = 'components/scripts/';
 jsonSrc = [outputDir + 'js/*.json'];
+fontSrc = [
+    'node_modules/font-awesome/fonts/*'
+];
 gulp.task('set-env-dev', function() {
     env = 'development';
     outputDir = 'builds/development/';
     sassStyle = 'expanded';
-
 });
-
 gulp.task('set-env-prod', function() {
     env = 'production';
     outputDir = 'builds/production/';
     sassStyle = 'compressed';
-
 });
-tsSrc = 'components/ts/';
-jsSrc = [
-    'components/scripts/site.js',
-];
-sassSrc = [
-  'components/sass/styles.scss'
-];
-
-//Bootstrap 4 JS Copy
-gulp.task('LibBootstrapJS', function() {
+//html
+gulp.task('html', function() {
+    gulp.src('builds/development/*.html')
+        .pipe(gulpif(env === 'production', minifyHTML()))
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+});
+//scss to css
+gulp.task('compass', function() {
+    gulp.src(sassSrc)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: outputDir + 'images',
+            style: sassStyle
+        })
+            .on('error', gutil.log))
+        .pipe(gulp.dest(outputDir + 'css'))
+});
+//Lib
+//Jquery
+gulp.task('LibJquery', function() {
     return gulp
         .src([
             'node_modules/jquery/dist/jquery.min.js',
+        ])
+        .pipe(gulp.dest(outputDir + 'js/lib/'))
+});
+//Bootstrap
+gulp.task('LibBootstrapJS', function() {
+    return gulp
+        .src([
             'node_modules/tether/dist/js/tether.min.js', //require for bootstrap
             'node_modules/bootstrap/dist/js/bootstrap.min.js'
         ])
         .pipe(gulp.dest(outputDir + 'js/lib/'))
 });
-//typescript to javascript
-gulp.task('typescript', function () {
-  return gulp
-      .src(tsSrc + '**/*.ts')
-      .pipe(sourcemaps.init())
-      .pipe(typescript(tscConfig.compilerOptions))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('components/scripts/'))
-      .pipe(gulp.dest(outputDir + 'js/'));
+//Font Awesome
+gulp.task('LibFontAwesomeCSS', function() {
+    return gulp
+        .src([
+            'node_modules/font-awesome/css/font-awesome.min.css'
+        ])
+        .pipe(gulp.dest(outputDir + 'css/'))
 });
 //combine javascript
 gulp.task('js', function() {
-  gulp.src(jsSrc)
-    .pipe(concat('script.js'))
-    .pipe(browserify())
-    .pipe(gulpif(env === 'production', uglify()))
-    .pipe(gulp.dest(outputDir + 'js'))
-});
-//sass to css
-gulp.task('compass', function() {
-  gulp.src(sassSrc)
-    .pipe(compass({
-      sass: 'components/sass',
-      image: outputDir + 'images',
-      style: sassStyle
-    })
-    .on('error', gutil.log))
-    .pipe(gulp.dest(outputDir + 'css'))
-});
-//html
-gulp.task('html', function() {
-  gulp.src('builds/development/*.html')
-      .pipe(gulpif(env === 'production', minifyHTML()))
-      .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    gulp.src(jsSrc + '**/*.js')
+        .pipe(concat('script.js'))
+        .pipe(browserify())
+        .pipe(gulpif(env === 'production', uglify()))
+        .pipe(gulp.dest(outputDir + 'js'))
 });
 //json
 gulp.task('json', function() {
-  gulp.src('builds/development/js/*.json')
-      .pipe(gulpif(env === 'production', jsonminify()))
-      .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
-      .on('error', gutil.log)
+    gulp.src('builds/development/js/*.json')
+        .pipe(gulpif(env === 'production', jsonminify()))
+        .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
+        .on('error', gutil.log)
+});
+//font
+gulp.task('font', function() {
+    gulp.src(fontSrc)
+        .pipe(gulp.dest(outputDir + 'fonts'))
 });
 //webserver for instant reload changes
 gulp.task('webserver', function() {
-  gulp.src('builds/development/')
-      .pipe(webserver({
-        livereload: true,
-        open: true
-      }));
+    gulp.src('builds/development/')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
 });
 //watch and reload
 gulp.task('watch', function() {
     gulp.watch('builds/development/*.html', ['html']);
     gulp.watch('components/sass/*.scss', ['compass']);
-    gulp.watch(tsSrc + '**/*.ts', ['typescript']);
-    gulp.watch(jsSrc, ['js']);
+    gulp.watch(jsSrc + '**/*.js', ['js']);
     gulp.watch('builds/development/js/*.json', ['json']);
 });
 
-gulp.task('libcopy', ['LibBootstrapJS']);
-gulp.task('common',['libcopy','typescript', 'js','compass' ,'html', 'json']);
-gulp.task('default', ['set-env-dev','common','webserver', 'watch']);
-gulp.task('build_for_prod', ['set-env-prod','common']);
+gulp.task('libcopy', ['LibJquery','LibBootstrapJS','LibFontAwesomeCSS']);
+gulp.task('necessary',['libcopy','html','compass','js','json','font']);
+gulp.task('default', ['set-env-dev','necessary','webserver', 'watch']);
+gulp.task('build_for_prod', ['set-env-prod','necessary']);
